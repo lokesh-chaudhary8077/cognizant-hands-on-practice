b25d34fc8bbbeee190ccd70005937eabddbc6c7d
Lokesh Chaudhary <lokeshchaudhary807760@gmail.com>
2026-07-01 13:29:39 +0530

    HasSufficientBalance

---
+++ PL-SQL Programming/Functions/Scenario3.sql
@@ -0,0 +1,29 @@
+-- HasSufficientBalance
+CREATE OR REPLACE FUNCTION HasSufficientBalance(
+    p_accountId NUMBER,
+    p_amount NUMBER
+)
+RETURN VARCHAR2
+IS
+    v_balance NUMBER;
+BEGIN
+
+    SELECT Balance
+    INTO v_balance
+    FROM Accounts
+    WHERE AccountID = p_accountId;
+
+    IF v_balance >= p_amount THEN
+        RETURN 'TRUE';
+    ELSE
+        RETURN 'FALSE';
+    END IF;
+
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RETURN 'ACCOUNT NOT FOUND';
+END;
+/
+
+SELECT HasSufficientBalance(1,500)
+FROM Dual;
