0ff6e00af41170c5181f5118177b87f47e441d6a
Lokesh Chaudhary <lokeshchaudhary807760@gmail.com>
2026-07-01 13:38:27 +0530

    CustomerManagement Package

---
+++ PL-SQL Programming/Packages/Scenario1.sql
@@ -0,0 +1,21 @@
+-- CustomerManagement Package
+CREATE OR REPLACE PACKAGE CustomerManagement AS
+
+PROCEDURE AddCustomer(
+p_id NUMBER,
+p_name VARCHAR2,
+p_dob DATE,
+p_balance NUMBER
+);
+
+PROCEDURE UpdateCustomer(
+p_id NUMBER,
+p_name VARCHAR2
+);
+
+FUNCTION GetBalance(
+p_id NUMBER
+) RETURN NUMBER;
+
+END CustomerManagement;
+/
