e9dc31a87caa943492447fefc2e58a089946d20f
Lokesh Chaudhary <lokeshchaudhary807760@gmail.com>
2026-07-01 13:18:03 +0530

    ProcessMonthlyInterest

---
+++ PL-SQL Programming/Stored Procedures/Scenario1.sql
@@ -0,0 +1,13 @@
+-- ProcessMonthlyInterest
+
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest
+AS
+BEGIN
+
+    UPDATE Accounts
+    SET Balance=Balance+(Balance*0.01)
+    WHERE AccountType='Savings';
+
+    COMMIT;
+END;
+/
