78653216fdafb742efd176ed83c83ac2be97c37a
Lokesh Chaudhary <lokeshchaudhary807760@gmail.com>
2026-07-01 13:27:01 +0530

    CalculateMonthlyInstallment

--- PL-SQL Programming/Functions/Scenario2.sql
+++ PL-SQL Programming/Functions/Scenario2.sql
@@ -1,1 +1,18 @@
--- 
+-- CalculateMonthlyInstallment
+CREATE OR REPLACE FUNCTION CalculateMonthlyInstallment(
+    p_loanAmount NUMBER,
+    p_interestRate NUMBER,
+    p_years NUMBER
+)
+RETURN NUMBER
+IS
+    v_installment NUMBER;
+BEGIN
+    v_installment :=
+        (p_loanAmount +
+        (p_loanAmount * p_interestRate * p_years / 100))
+        / (p_years * 12);
+
+    RETURN v_installment;
+END;
+/
